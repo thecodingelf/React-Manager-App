da5927fe937b9102f56006917cc5e94ceebe239a
Jukka Koivu <c2koju00@students.oamk.fi>
2018-02-01 15:34:45 +0200

    added project to github

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from '../manager/src/app/App';
+
+AppRegistry.registerComponent('manager', () => App);
---
+++ src/app/Credentials.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDySveDkcTg7mwUYhZNugqf4ATPvHN_b-g",
+    authDomain: "manager-7160e.firebaseapp.com",
+    databaseURL: "https://manager-7160e.firebaseio.com",
+    projectId: "manager-7160e",
+    storageBucket: "manager-7160e.appspot.com",
+    messagingSenderId: "980691889387"
+};
